c7cdd901d18ffc6e8cf13638a07d249c25d4e33b
apardo04 <apardo04@gmail.com>
2019-05-23 20:55:56 -0400

    changing classes to hooks!

--- components/SeeMore.js
+++ components/SeeMore.js
@@ -1,76 +1,75 @@
-class SeeMore extends React.Component {
-    render() {
-        return (
-            <div className="see-more" id="lead-see-more">
-                <span>
-                    <a href={this.props.href} className="see-more-arrows">
-                        <i className="fa fa-angle-down" aria-hidden="true"></i>
-                        <i className="fa fa-angle-down" aria-hidden="true"></i>
-                        <i className="fa fa-angle-down" aria-hidden="true"></i>
-                        <span className="hidden">Jump to next section</span>
-                    </a>
-                </span>
-                <style jsx>{`
-                    @keyframes opac {
-                        from {opacity: .4;}
-                        to {opacity: 1;}
-                    }
+const SeeMore = props => {
+    return (
+        <div className="see-more" id="lead-see-more">
+            <span>
+                <a href={props.href} className="see-more-arrows">
+                    <i className="fa fa-angle-down" aria-hidden="true"></i>
+                    <i className="fa fa-angle-down" aria-hidden="true"></i>
+                    <i className="fa fa-angle-down" aria-hidden="true"></i>
+                    <span className="hidden">Jump to next section</span>
+                </a>
+            </span>
+            <style jsx>{`
+                @keyframes opac {
+                    from {opacity: .4;}
+                    to {opacity: 1;}
+                }
+                .see-more {
+                    position: relative;
+                    width: 100%;
+                    font-size: 4.5em;
+                    z-index: 10;
+                    top: 85%;
+                }
+                .see-more span {
+                    cursor: pointer;
+                    display: block;
+                    margin: 0 auto;
+                    width: 35px;
+                    height: 35px;
+                    border-radius: 50%;
+                    text-align: center;
+                }
+                .see-more-arrows {
+                    left: 4rem;
+                    bottom: 4rem;
+                }
+                .see-more-arrows .fa {
+                    color: var(--yellow);
+                    display: block;
+                    line-height: .2;
+                    animation-name: opac;
+                    animation-duration: 1.2s;
+                    animation-timing-function: linear;
+                    animation-delay: 0s;
+                    animation-iteration-count: infinite;
+                    animation-direction: normal;
+                    animation-fill-mode: none;
+                    animation-play-state: running;
+                }
+                .see-more-arrows .fa:nth-child(2) {
+                    animation-delay: 0.4s;
+                }
+                .see-more-arrows .fa:nth-child(3) {
+                    animation-delay: 0.8s;
+                }
+                .see-more-arrows:hover {
+                    text-decoration: none;
+                }
+                .see-more-arrows:hover .fa, .see-more-arrows:active .fa, .see-more-arrows:focus .fa {
+                    -webkit-animation: none;
+                    animation: none;
+                }
+                @media only screen and (max-width: 992px) {
                     .see-more {
-                        position: relative;
-                        width: 100%;
-                        font-size: 4.5em;
-                        z-index: 10;
-                        top: 85%;
+                        top: 50%;
                     }
-                    .see-more span {
-                        cursor: pointer;
-                        display: block;
-                        margin: 0 auto;
-                        width: 35px;
-                        height: 35px;
-                        border-radius: 50%;
-                        text-align: center;
-                    }
-                    .see-more-arrows {
-                        left: 4rem;
-                        bottom: 4rem;
-                    }
-                    .see-more-arrows .fa {
-                        color: var(--yellow);
-                        display: block;
-                        line-height: .2;
-                        animation-name: opac;
-                        animation-duration: 1.2s;
-                        animation-timing-function: linear;
-                        animation-delay: 0s;
-                        animation-iteration-count: infinite;
-                        animation-direction: normal;
-                        animation-fill-mode: none;
-                        animation-play-state: running;
-                    }
-                    .see-more-arrows .fa:nth-child(2) {
-                        animation-delay: 0.4s;
-                    }
-                    .see-more-arrows .fa:nth-child(3) {
-                        animation-delay: 0.8s;
-                    }
-                    .see-more-arrows:hover {
-                        text-decoration: none;
-                    }
-                    .see-more-arrows:hover .fa, .see-more-arrows:active .fa, .see-more-arrows:focus .fa {
-                        -webkit-animation: none;
-                        animation: none;
-                    }
-                    @media only screen and (max-width: 992px) {
-                        .see-more {
-                            top: 50%;
-                        }
-                    }
-                `}</style>
-            </div>
-            
-        )
-    }
+                }
+            `}</style>
+        </div>
+        
+    )
 }
+
 
 export default SeeMore
--- components/nav.js
+++ components/nav.js
@@ -1,197 +1,189 @@
 import Link from 'next/link'
+import React, { useState, useEffect } from 'react'
 
-class Nav extends React.Component {
-  constructor(props) {
-    super(props)
-      this.state = {visible: false}
-      this.toggleShow = this.toggleShow.bind(this)
-  }
-  toggleShow() {
-    this.setState({ visible: !this.state.visible });
-  }
-  render() {
-    const { visible } = this.state;
-    return (
-      <React.Fragment>
-        <div id="mobile-menu-open" className="shadow-large" onClick={this.toggleShow}>
-          <i className="fa fa-bars" aria-hidden="true" id="menu-bars"></i>
-        </div>
-        <nav onClick={this.toggleShow} className={ visible ? "active" : undefined }>
-          {/* End #mobile-menu-toggle */}
-          <div id="mobile-menu-close">
-                <span>Close</span><i className="fa fa-times" aria-hidden="true"></i>
-            </div>
-            <ul id="menu">
-                <li>
-                {this.props.page == "index" ?
-                  <Link href="/index#about">
-                    <a tabIndex="0" className="hover-border prevent-default">About</a>
+const Nav = props => {
+  const [mobileMenu, toggleMobileMenu] = useState(false);
+
+  return (
+    <React.Fragment>
+      <div id="mobile-menu-open" className="shadow-large" onClick={() => toggleMobileMenu(!mobileMenu)}>
+        <i className="fa fa-bars" aria-hidden="true" id="menu-bars"></i>
+      </div>
+      <nav onClick={() => toggleMobileMenu(!mobileMenu)} className={ mobileMenu ? "active" : undefined }>
+        {/* End #mobile-menu-toggle */}
+        <div id="mobile-menu-close">
+              <span>Close</span><i className="fa fa-times" aria-hidden="true"></i>
+          </div>
+          <ul id="menu">
+              <li>
+              {props.page == "index" ?
+                <Link href="/index#about">
+                  <a tabIndex="0" className="hover-border prevent-default">About</a>
+                </Link>
+              :     
+                <Link href="/index">
+                  <a tabIndex="0" className="hover-border prevent-default">Home</a>
+                </Link>
+              }
+              </li>
+              <li>
+                <Link href="/index#projects">
+                  <a tabIndex="0" className="hover-border prevent-default">Projects</a>
+                </Link>
+              </li>
+              <li>
+                  <Link href="/blog">
+                    <a tabIndex="0" className="hover-border prevent-default">Blog</a>
                   </Link>
-                :     
-                  <Link href="/index">
-                    <a tabIndex="0" className="hover-border prevent-default">Home</a>
+              </li>
+              <li>
+                  <Link href="/hire-me">
+                    <a tabIndex="0" className="hover-border">Need a Developer?</a>
                   </Link>
-                }
-                </li>
-                <li>
-                  <Link href="/index#projects">
-                    <a tabIndex="0" className="hover-border prevent-default">Projects</a>
-                  </Link>
-                </li>
-                <li>
-                    <Link href="/blog">
-                      <a tabIndex="0" className="hover-border prevent-default">Blog</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/hire-me">
-                      <a tabIndex="0" className="hover-border">Need a Developer?</a>
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-        <style jsx>{`
-          nav {
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
+              </li>
+          </ul>
+      </nav>
+      <style jsx>{`
+        nav {
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          text-align: center;
+          z-index: 10;
+          -webkit-animation-name: dropHeader;
+          animation-name: dropHeader;
+          -webkit-animation-iteration-count: 1;
+          animation-iteration-count: 1;
+          -webkit-animation-timing-function: ease;
+          animation-timing-function: ease;
+          -webkit-animation-duration: 0.75s;
+          animation-duration: 0.75s;
+        }
+        nav ul {
+            display: inline-block;
+            /*background: var(--darkblue);*/
             text-align: center;
-            z-index: 10;
-            -webkit-animation-name: dropHeader;
-            animation-name: dropHeader;
-            -webkit-animation-iteration-count: 1;
-            animation-iteration-count: 1;
-            -webkit-animation-timing-function: ease;
-            animation-timing-function: ease;
-            -webkit-animation-duration: 0.75s;
-            animation-duration: 0.75s;
+            padding: 10px;
+            margin: 0;
+            border-bottom-right-radius: 4px;
+            border-bottom-left-radius: 4px;
+        }
+        nav li {
+            display: inline-block;
+        }
+        nav a {
+            display: block;
+            color: var(--yellow);
+            padding: 10px;
+            font-size: 1.1em;
+            font-weight: 100;
+        }
+        nav a:focus {
+            color: var(--blue);
+            text-decoration: none;
+        }
+        nav.active {
+            display: block;
+        }
+        nav.sticky {
+            position: fixed;
+            z-index: 999;
+        }
+        #menu.active {
+            display: block;
+        }
+        .hover-border:hover {
+            text-decoration: none;
+        }
+        .hover-border:after {
+            content: '';
+            display: block;
+            width: 100%;
+            border-bottom: 4px solid var(--blue);
+            border-right: 4px solid transparent;
+            border-left: 4px solid transparent;
+            -webkit-transition: -webkit-transform 0.4s;
+            transition: transform .4s;
+            -webkit-transform: scale(0,1);
+            transform: scale(0,1);
+        }
+        .hover-border:hover:after {
+            -webkit-transform: scale(1);
+            transform: scale(1);
+        }
+        #mobile-menu-open {
+            display: none;
+            cursor: pointer;
+            position: sticky;
+            right: 15px;
+            top: 10px;
+            color: var(--purple2);
+            font-size: 1.5em;
+            z-index: 20;
+            border-radius: 4px;
+            background: var(--darkblue);
+        }
+        #mobile-menu-close {
+            display: none;
+            text-align: right;
+            width: 100%;
+            background: var(--darkblue);
+            font-size: 1.5em;
+            padding-right: 15px;
+            padding-top: 10px;
+            cursor: pointer;
+            color: var(--purple2);
+        }
+        #mobile-menu-close span {
+            font-size: 0.8em;
+            text-transform: uppercase;
+        }
+        #mobile-menu-close i {
+            vertical-align: middle;
+            font-size: 1.2em;
+        }
+        @media only screen and (max-width: 768px) {
+          nav {
+              position: fixed;
+              display: none;
+              z-index: 999;
+              -webkit-animation: none;
+              animation: none;
+              bottom: 0;
+              height: 100%;
           }
-          nav ul {
-              display: inline-block;
-              /*background: var(--darkblue);*/
-              text-align: center;
-              padding: 10px;
-              margin: 0;
-              border-bottom-right-radius: 4px;
-              border-bottom-left-radius: 4px;
-          }
-          nav li {
-              display: inline-block;
-          }
-          nav a {
+          #mobile-menu-open, #mobile-menu-close {
               display: block;
               color: var(--yellow);
-              padding: 10px;
-              font-size: 1.1em;
-              font-weight: 100;
-          }
-          nav a:focus {
-              color: var(--blue);
-              text-decoration: none;
-          }
-          nav.active {
-              display: block;
-          }
-          nav.sticky {
-              position: fixed;
-              z-index: 999;
-          }
-          #menu.active {
-              display: block;
-          }
-          .hover-border:hover {
-              text-decoration: none;
-          }
-          .hover-border:after {
-              content: '';
-              display: block;
               width: 100%;
-              border-bottom: 4px solid var(--blue);
-              border-right: 4px solid transparent;
-              border-left: 4px solid transparent;
-              -webkit-transition: -webkit-transform 0.4s;
-              transition: transform .4s;
-              -webkit-transform: scale(0,1);
-              transform: scale(0,1);
+              border-bottom: 3px solid var(--yellow);
+              height: 50px;
+              top: 0;
           }
-          .hover-border:hover:after {
-              -webkit-transform: scale(1);
-              transform: scale(1);
-          }
-          #mobile-menu-open {
-              display: none;
-              cursor: pointer;
-              position: sticky;
-              right: 15px;
-              top: 10px;
-              color: var(--purple2);
-              font-size: 1.5em;
-              z-index: 20;
-              border-radius: 4px;
-              background: var(--darkblue);
-          }
-          #mobile-menu-close {
-              display: none;
-              text-align: right;
+          #menu {
+              height: 100%;
+              overflow-y: auto;
+              -webkit-box-shadow: none;
+              box-shadow: none;
+              border-radius: 0;
               width: 100%;
               background: var(--darkblue);
-              font-size: 1.5em;
-              padding-right: 15px;
-              padding-top: 10px;
-              cursor: pointer;
-              color: var(--purple2);
           }
-          #mobile-menu-close span {
-              font-size: 0.8em;
-              text-transform: uppercase;
+          #menu-bars {
+              padding-top: 12px;
+              font-size: 23pt;
+              right: 5vw;
+              position: absolute;
           }
-          #mobile-menu-close i {
-              vertical-align: middle;
-              font-size: 1.2em;
+          #menu li {
+              display: block;
+              margin-bottom: 10px;
           }
-          @media only screen and (max-width: 768px) {
-            nav {
-                position: fixed;
-                display: none;
-                z-index: 999;
-                -webkit-animation: none;
-                animation: none;
-                bottom: 0;
-                height: 100%;
-            }
-            #mobile-menu-open, #mobile-menu-close {
-                display: block;
-                color: var(--yellow);
-                width: 100%;
-                border-bottom: 3px solid var(--yellow);
-                height: 50px;
-                top: 0;
-            }
-            #menu {
-                height: 100%;
-                overflow-y: auto;
-                -webkit-box-shadow: none;
-                box-shadow: none;
-                border-radius: 0;
-                width: 100%;
-                background: var(--darkblue);
-            }
-            #menu-bars {
-                padding-top: 12px;
-                font-size: 23pt;
-                right: 5vw;
-                position: absolute;
-            }
-            #menu li {
-                display: block;
-                margin-bottom: 10px;
-            }
-          }
-        `}</style>
-      </React.Fragment>
-    )
-  }
+        }
+      `}</style>
+    </React.Fragment>
+  )
 }
 
 export default Nav
