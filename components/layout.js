977087dd96366a259bde2dfd6d1261fb68b45326
apardo04 <apardo04@gmail.com>
2019-06-04 13:34:57 -0400

    adding google analytics

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,27 @@
 import Head from './head'
 import Nav from '../components/nav'
 import Footer from '../components/footer'
+import React, { useEffect } from 'react';
 import { string } from 'prop-types'
+import { initGA, logPageView } from '../utils/analytics'
 
-const Layout = props => (
-    <React.Fragment>
-        <Head page={props.page} title={props.title} description={props.description} />
-        <Nav page={props.page} />
-        {props.children}
-        <Footer />
-    </React.Fragment>
-)
+const Layout = props => {
+    useEffect(() => {
+        if (!window.GA_INITIALIZED && process.env.NODE_ENV == 'production') {
+            initGA()
+            window.GA_INITIALIZED = true
+        }
+        logPageView()
+      });
+    return (
+        <React.Fragment>
+            <Head page={props.page} title={props.title} description={props.description} />
+            <Nav page={props.page} />
+            {props.children}
+            <Footer />
+        </React.Fragment>
+    )
+}
 
 Layout.propTypes = {
     title: string,
