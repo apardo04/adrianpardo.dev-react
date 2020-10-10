7b1b5c776756e989831f0c1b8f0673260cac4625
Adrian Pardo <apardo@Adrians-MacBook-Pro.local>
2020-10-10 11:55:02 -0400

    Updating next.js and about html/css

--- components/About.js
+++ components/About.js
@@ -9,27 +9,21 @@ const About = props => {
                     <div className="col-md-4">
                         <h2 className="heading" tabIndex="0">About Me</h2>
                         <div>
-                            <img className="cert" id="nike-cert" alt="nike-2020-intern-badge" src="../assets/images/2020-nike-intern-logo.png" />
-                        </div>
-                        <div>
                             <a href="https://www.youracclaim.com/badges/a88f8719-21de-49fd-b869-fcd470ccb18d/public_url" target="_blank" rel="noopener">
-                                <img className="cert" alt="aws-cloud-practitioner-badge" src="../assets/images/aws-certified-cloud-practitioner.png" />
+                                <img className="cert" id="aws-badge" alt="aws-cloud-practitioner-badge" src="../assets/images/aws-certified-cloud-practitioner.png" />
                             </a>
                         </div>
+                        <div>
+                            <img className="cert" id="nike-badge" alt="nike-2020-intern-badge" src="../assets/images/2020-nike-intern-logo.png" />
+                        </div>
                     </div>
                     <div className="col-md-8">
-                    <p>
-                        I'm an experienced software engineer based in Miami, Fl.<br /><br />
-                        I have developed, launched, and maintained <a href="#projects">multiple projects</a> from scratch.<br /><br />
-                        Some of my strongest <a href="#skills">skills</a> include web development, cloud computing & e-commerce.<br /><br />
-                        I'm always looking to collaborate on interesting projects. Need a developer? - <Link href="#contact"><a className="underline-yellow">contact me</a></Link>
-                    </p>
-                    </div>
-                </div>
-                <div className="row padding-top">
-                    <div className="col-md-4">
-                    </div>
-                    <div className="col-md-8">
+                        <p id="about-text">
+                            I'm an experienced software engineer based in Miami, Fl.<br /><br />
+                            I have developed, launched, and maintained <a href="#projects">multiple projects</a> from scratch.<br /><br />
+                            Some of my strongest <a href="#skills">skills</a> include web development, cloud computing & e-commerce.<br /><br />
+                            I'm always looking to collaborate on interesting projects. Need a developer? - <Link href="#contact"><a className="underline-yellow">contact me</a></Link>
+                        </p>
                         <div className="education-block">
                             <h3>Florida International University</h3>
                             <h4>Bachelor of Science in Computer Science</h4>
@@ -37,8 +31,7 @@ const About = props => {
                         <div className="education-block">
                             <h3>Miami Dade College</h3>
                             <h4>Associates in Arts Music Business: Music Production</h4>
-                        </div>    
-             
+                        </div>  
                     </div>
                 </div>
             </div>
@@ -47,11 +40,15 @@ const About = props => {
                 .cert {
                     width: 45%;
                 }
-                #nike-cert {
-                    background: white;
-                    border: 1px solid #030c22;
-                    border-radius: 18px;
+                #aws-badge {
                     margin-bottom: 15px;
+                }
+                #nike-badge {
+                    background: white;
+                    border-radius: 4px;
+                }
+                #about-text {
+                    margin-bottom: 50px;
                 }
                 @media only screen and (max-width: 768px) {
                     .cert {
