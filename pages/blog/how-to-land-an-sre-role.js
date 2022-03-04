896db8521c077206ba791619a3a071ee3fbc9db7
apardo04 <apardo04@gmail.com>
2022-03-04 13:53:30 -0500

    adding more content; fixing mobile lead image

--- pages/blog/how-to-land-an-sre-role.js
+++ pages/blog/how-to-land-an-sre-role.js
@@ -26,7 +26,7 @@ export default function Post() {
             <div className="info-container-2 text-center">
                 <div className="container info-blog">
                     <div>
-                        <h2 className="heading" tabIndex="0" id="contents">Contents</h2>
+                        <h2 className="heading" tabIndex="0" id="contents">Contents</h2> 
                         <ul className="blog-ul">
                             <li className="contents-list"><a href="#cloud-certificate">Cloud Certificate</a></li>
                             <li className="contents-list"><a href="#operating-systems">Operating Systems</a></li>
@@ -34,10 +34,10 @@ export default function Post() {
                             <li className="contents-list"><a href="#docker">Docker</a></li>
                             <li className="contents-list"><a href="#terraform">Terraform</a></li>
                             <li className="contents-list"><a href="#ci-cd">CI/CD</a></li>
-                            <li className="contents-list"><a href="#monitoring-logging-alerting">Monitoring, Logging, Alerting</a></li>
-                            <li className="contents-list"><a href="#programming-language">Programming Language</a></li>
+                            <li className="contents-list"><a href="#monitoring-logging-alerting-outages">Monitoring, Logging, Alerting & Outages</a></li>  
                             <li className="contents-list"><a href="#sre-interview">The Interview - How to Prepare</a></li>
                         </ul>
+                        <p>*This post is a work in progress, but may still be useful as is. Please be patient.</p>
                     </div>
 
                     <div>
@@ -57,25 +57,25 @@ export default function Post() {
                     </div>
                     <div>
                         <h2 className="heading" tabIndex="0" id="operating-systems">Operating Systems</h2>
-                        <p>No two SRE roles are created equal. Most will have you working on Linux Systems, some Windows, some both.</p>
+                        <p>No two SRE roles are created equal. Most will have you working on Linux Systems, and some Windows/both. </p>
 
                         <h3 className="blog-heading yellow-border white">Scripting Languages</h3>
                         <p><a href="https://www.learnshell.org/" target="new" rel="noopener">Bash</a> is the command shell and scripting language for the majority of Linux systems.<br/>
                         While <a href="https://docs.microsoft.com/en-us/learn/paths/powershell/" target="new" rel="noopener">PowerShell</a> is the command shell and scripting language for the majority of Windows systems.</p>
                         
 
-                        <p>Some of the most important concepts to know are Process Management, Networking, Threads/Concurrency, Text Manipulation Tools, I/O Management, Virtualization, Memory storage, and File systems.</p>
+                        <p>Some of the most important concepts to know and be able to troubleshoot are <a href="https://www.redhat.com/sysadmin/beginners-guide-network-troubleshooting-linux" target="new" rel="noopener">Networking</a>, Process Management, Threads/Concurrency, I/O Management, Virtualization, Memory storage, File systems, etc.</p>
                     </div>
                     <div>
                         <h2 className="heading" tabIndex="0" id="networking">Networking</h2>
-                        <p>This is a very broad topic, but some of the most important concepts are Virtual Networks, <a href="https://www.learningjournal.guru/article/public-cloud-infrastructure/what-is-bastion-host-server/" target="new" rel="noopener">Bastions</a></p> 
+                        <p>This is a very broad topic, but some of the most important concepts are <a href="https://howdns.works/" target="new" rel="noopener">DNS</a>, <a href="https://howhttps.works/why-do-we-need-https/" target="new" rel="noopener">HTTPS</a>, Virtual Networks, Network Security Groups, <a href="https://www.learningjournal.guru/article/public-cloud-infrastructure/what-is-bastion-host-server/" target="new" rel="noopener">Bastions</a></p> 
                         <p>It is good to be familiar with the <a href="https://www.cloudflare.com/learning/ddos/glossary/open-systems-interconnection-model-osi/" target="new" rel="noopener">OSI Model</a>. It is the universal language for computer networking. It splits up the communication system into seven abstract layers. This can be useful when debugging networking issues.</p>
                     </div>
                     <div>
                         <h2 className="heading" tabIndex="0" id="docker">Docker</h2>
                         <p>In reality, this section is also about <a href="https://www.docker.com/resources/what-container" target="new" rel="noopener">containers</a>.</p>
                         <p>Best thing you can do is, install <a href="https://www.docker.com/products/docker-desktop" target="new" rel="noopener">Docker Desktop</a> and find a <a href="https://docs.docker.com/get-started/" target="new" rel="noopener">tutorial like this one</a> to get you started.</p>
-                        <p>Once you're somewhat comfortable, take it to the next level by installing Docker on an AWS or Azure instance and run some sort of web application on it. See if you can access the web application from a public address. Doing this will start to combine everything you've learned so far together. This is the kind of experimenting that will begin to give you the experience needed for Cloud based role.</p>
+                        <p>Once you're somewhat comfortable, take it to the next level by installing Docker on an AWS or Azure instance and run some sort of web application on it. See if you can access the web application from a public address. Doing this will start to combine everything you've learned so far together. This is the kind of experimenting that will begin to give you the experience needed for a Cloud based role.</p>
                         
                     </div>
                     <div>
@@ -85,17 +85,21 @@ export default function Post() {
                     </div>
                     <div>
                         <h2 className="heading" tabIndex="0" id="ci-cd">CI/CD</h2>
-                        <p></p>
+                        <p><a href="https://www.redhat.com/en/topics/devops/what-is-ci-cd" target="new" rel="noopener">What is CI/CD?</a></p>
                     </div>
                     <div>
-                        <h2 className="heading" tabIndex="0" id="monitoring-logging-alerting">Monitoring, Logging, Alerting</h2>
-                        <p></p>
+                        <h2 className="heading" tabIndex="0" id="monitoring-logging-alerting-outages">Monitoring, Logging, Alerting, Outages</h2>
+                        <p>Look into services like Splunk, Datadog & Elastic Stack, which are used for Log Analysis, Monitoring and Management.</p>
+                        <p><a href="https://www.pagerduty.com/resources/learn/what-is-slo-sla-sli/" target="new" rel="noopener">SLI/SLO</a></p>
+                        <p><a href="https://www.linkedin.com/pulse/how-perform-incident-post-mortems-identify-root-cause-s%C3%B8ren-pedersen/" target="new" rel="noopener">RCA/Post Mortem</a></p>
+                        <p>There is a great podcast called <a href="https://downtimeproject.com/" target="new" rel="noopener">The Downtime Project</a> where they go analyze Outages, RCA's & Post Mortem released by major companies.</p>
                     </div>
                     <div>
                         <h2 className="heading" tabIndex="0" id="sre-interview">Interview - How to Prepare</h2>
                         <p>Besides being moderately comfortable with everything mentioned above, there a few things you can do to better prepare for an SRE interview.</p>
                         <p>You should read the entire <a href="https://sre.google/sre-book/table-of-contents/" target="new" rel="noopener">Google SRE Book</a> and <a href="https://sre.google/workbook/table-of-contents/" target="new" rel="noopener">Google SRE Workbook</a>, these are the unofficial SRE bibles.</p>
                         <p>Sites like Leetcode are great for the coding part of the interview. They also have problem sets on <a href="https://leetcode.com/problemset/concurrency/Concurrency" target="new" rel="noopener">Concurrency/Race Conditions</a>, which are typically seen in SRE interviews.</p>
+                        <p>This is a great <a href="https://discord.gg/kr869yU9" target="new" rel="noopener">SRE Discord</a>. It even has a channel dedicated to interviewing questions.</p>
                     </div> 
                     <Disqus.DiscussionEmbed
                         shortname={disqusShortname}
@@ -110,6 +114,11 @@ export default function Post() {
                 }
                 .blog-ul {
                     width: 350px;
+                }
+                @media only screen and (max-width: 992px) {
+                    .lead {
+                        background-size: contain;
+                    }
                 }
         `}</style>
         </Layout>
